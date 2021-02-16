c242c3db4bedaae39ea28c7787522c6a8885bded
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2021-02-16 11:16:31 +0700

    config & database sebelum gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,10 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/smd';
-// $config['base_url'] = 'http://192.168.100.50/smd';
-$config['base_url'] = 'http://192.168.100.225/smd';
-// $config['base_url'] = 'http://sikeren.com';
+// $config['base_url'] = 'http://localhost/sikeren';
+// $config['base_url'] = 'http://192.168.100.50/sikeren';
+// $config['base_url'] = 'http://192.168.100.225/sikeren';
+$config['base_url'] = 'http://sikeren.com';
 
 /*
 |--------------------------------------------------------------------------
