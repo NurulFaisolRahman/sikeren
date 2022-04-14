fc63e6cfb7692653805456e95d6a2eed4045a54c
nurulfaisolrahman <nurulfaisolrahman@gmail.com>
2022-04-14 13:30:52 +0700

    Subdomain Akuntansi V2

--- Akuntansi/application/controllers/SMD.php
+++ Akuntansi/application/controllers/SMD.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class SMD extends CI_Controller {
 
 	public function index(){
-		$this->load->view('Index.php');
+		$this->load->view('Index.php'); 
 	}
 
 	public function MhsMasuk(){
